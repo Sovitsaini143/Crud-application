7146aa994481b5d240c03d1f7a9bfe8183a93b87
Sovitsaini143 <sovitsaini37@gmail.com>
2024-10-08 07:56:51 +0530

    crud app

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,12 +1,12 @@
 import express from "express"
 
-import { create, deleteUser, getAllUsers, gretUserById, update } from  "../controller/userController.js"
+import { create, deleteUser, getAllUsers, getUserById, update } from  "../controller/userController.js";
 
 const route = express.Router();
 
 route.post("/user", create)
 route.get("/users", getAllUsers)
-route.get("/user/:id", gretUserById)
+route.get("/user/:id", getUserById)
 route.put("/update/user/:id",update)
 route.delete("/delete/user/:id",deleteUser)
 
